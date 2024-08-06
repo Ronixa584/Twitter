f4cc1ec0ed22a76153ae795a8a0f098559379adf
Ronixa584 <asp15299@gmail.com>
2024-08-07 00:48:34 +0530

    Created a frontend of SignIn

--- twitter-client/app/layout.tsx
+++ twitter-client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +16,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <GoogleOAuthProvider clientId="611174461284-a5h5vv753m7o9eq0ri5c8hekkgeeo6cm.apps.googleusercontent.com">
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </GoogleOAuthProvider>
   );
 }
--- twitter-client/app/page.tsx
+++ twitter-client/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { BsTwitter } from "react-icons/bs";
 import { BiSolidHomeCircle } from "react-icons/bi";
 import { IoSearchSharp } from "react-icons/io5";
@@ -7,9 +9,10 @@ import { LuSquareSlash } from "react-icons/lu";
 import { BsFillPeopleFill } from "react-icons/bs";
 import { IoPerson } from "react-icons/io5";
 import { CiCircleMore } from "react-icons/ci";
-import React from "react";
+import React, { useCallback } from "react";
 import FeedCard from "./Componenet/FeedCard";
 import PostTweet from "./Componenet/PostTweet";
+import { CredentialResponse, GoogleLogin, GoogleOAuthProvider } from "@react-oauth/google";
 
 interface TwitterSidebarButton {
   title: string;
@@ -53,46 +56,57 @@ const listOfMenu: TwitterSidebarButton[] = [
     title: "More",
     icon: <CiCircleMore className="text-3xl" />,
   },
-]; 
+];
 
 export default function Home() {
+
+  const handleLoginWithGoogle = useCallback((cred: CredentialResponse) => {
+
+  }, [])
+
   return (
-    <div className="main flex h-screen w-screen justify-center">
-      <div className="first w-1/4 ">
-        <div className="menu flex flex-col ml-20 p-1">
-          <div className="logo hover:bg-gray-800 rounded-full flex justify-center items-center w-14 h-14 ml-1">
-            <BsTwitter className="text-3xl" />
-          </div>
-          {listOfMenu.map((item) => (
-            <div
-              key={item.title}
-              className="menuElements flex gap-5 hover:bg-gray-800 rounded-full w-fit cursor-pointer px-5 py-2"
-            >
-              <div className="">{item.icon}</div>
-              <div className="flex justify-center items-center text-xl font-semibold">
-                {item.title}
-              </div>
+      <div className="main flex h-screen w-screen justify-center">
+        <div className="first w-1/4 ">
+          <div className="menu flex flex-col ml-20 p-1">
+            <div className="logo hover:bg-gray-800 rounded-full flex justify-center items-center w-14 h-14 ml-1">
+              <BsTwitter className="text-3xl" />
             </div>
-          ))}
+            {listOfMenu.map((item) => (
+              <div
+                key={item.title}
+                className="menuElements flex gap-5 hover:bg-gray-800 rounded-full w-fit cursor-pointer px-5 py-2"
+              >
+                <div className="">{item.icon}</div>
+                <div className="flex justify-center items-center text-xl font-semibold">
+                  {item.title}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="mt-5 pl-24 pr-10">
+            <button className="tweetButton bg-[#1d9bf0] font-semibold py-3 rounded-full w-full">
+              Tweet
+            </button>
+          </div>
+          <div className="profile"></div>
         </div>
-        <div className="mt-5 pl-24 pr-10">
-          <button className="tweetButton bg-[#1d9bf0] font-semibold py-3 rounded-full w-full">
-            Tweet
-          </button>
+        <div className="second  border border-gray-700 w-2/5 h-screen overflow-y-scroll no-scrollbar">
+          <PostTweet />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
+          <FeedCard />
         </div>
-        <div className="profile"></div>
+        <div className="third w-1/4">
+          <div className="border border-3 pt-8 pl-8 pb-8">
+            <h1 className="text-2xl font-bold">New to Twitter? Join today!</h1>
+            <div className="mt-5">
+            <GoogleLogin onSuccess={(cred) => console.log(cred)} />
+              </div>
+          </div>
+        </div>
       </div>
-      <div className="second  border border-gray-700 w-2/5 h-screen overflow-y-scroll no-scrollbar">
-      <PostTweet/>
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-        <FeedCard />
-      </div>
-      <div className="third w-1/4"></div>
-    </div>
   );
 }
-
